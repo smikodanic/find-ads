a13e9442e6a32b544c27ae3bdcb273eb69b2a24a
smikodanic <smikodanic@gmail.com>
2015-04-21 11:20:06 +0200

    link crawling finished

--- 0crawler/nodejs/tests/01childprocess_exec_pwd.js
+++ 0crawler/nodejs/tests/01childprocess_exec_pwd.js
@@ -1,7 +1,6 @@
 /*jslint unparam: true*/
 
 var childProcess = require('child_process');
-var MongoClient = require('mongodb').MongoClient;
 
 childProcess.exec('pwd', function (err, stdout, stderr) {
   console.log(stdout);
