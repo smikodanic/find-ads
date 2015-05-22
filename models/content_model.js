a69581666fc622b7e9d9a168553e22b62ec1b6c0
smikodanic <smikodanic@gmail.com>
2015-05-22 12:41:20 +0200

    test one url for content crawling

--- models/content_model.js
+++ models/content_model.js
@@ -11,35 +11,45 @@ var pagination = require('libraries/pagination.js');
 //mongo parameters
 var settings = require('settings/admin.js');
 var dbName = settings.mongo.dbName;
-var collName_cnt = settings.mongo.dbColl_cnt;
 
 /**
- * Insert content into 'content' collection when crawling
+ * Insert content into 'content_*' collection when crawling
  * @param  {String} pageURL  - url of pagination link: http://www.adsuu.com/business-offer-9/
  * @param  {Object} db       - db mongo object
- * @param  {String} collName - MongoDB collection name: linkQueue_adsuu_com_business
+ * @param  {String} contentCollection - MongoDB collection name: content_business
  * @param  {Object} insMoDoc - MongoDB document to be inserted: {_id, task_is, page, dateTime, links[]}
  * @return {[type]}          [description]
  */
-module.exports.insertContent = function (pageURL, db, collName, insMoDoc) {
+module.exports.insertContent = function (pageURL, db, contentCollection, insMoDoc) {
 
-  db.collection(collName_cnt).createIndex({page: 1}, {unique: true, sparse: true}, function (err) { //create unique index to prevent duplicated documents
-    if (err) { logg.me('error', __filename + ':27 ' + err); }
+  db.collection(contentCollection).createIndex({page: 1}, {unique: true, sparse: true}, function (err) { //create unique index to prevent duplicated documents
+    if (err) { logg.me('error', __filename + ':26 ' + err); }
 
-    db.collection(collName).find({"pageURL": pageURL}).toArray(function (err, moTask2_arr) { //check if collection e.g. pageURL already exists
-      if (err) { logg.me('error', __filename + ':30 ' + err); }
+    db.collection(contentCollection).find({"pageURL": pageURL}).toArray(function (err, moContent_arr) { //check if doc e.g. pageURL already exists
+      if (err) { logg.me('error', __filename + ':29 ' + err); }
 
       //if collection already exists do UPDATE
-      if (moTask2_arr.length !== 0) {
+      if (moContent_arr.length !== 0) {
 
-        db.collection(collName).update({"page": pageURL}, insMoDoc, function (err) {
-          if (err) { logg.me('error', __filename + ':36 ' + err); }
+        db.collection(contentCollection).update({"page": pageURL}, insMoDoc, function (err) {
+          if (err) { logg.me('error', __filename + ':35 ' + err); }
         });
 
       } else { //if collection doesn't exist do INSERT
 
-        db.collection(collName).insert(insMoDoc, function (err) {
-          if (err) { logg.me('error', __filename + ':42 ' + err); }
+        db.collection(contentCollection).find().sort({cid: -1}).limit(1).toArray(function (err, moDocs_arr) { //find max ID
+          if (err) { logg.me('error', __filename + ':41 ' + err); }
+
+          //define new insDoc.id from max id value
+          if (moDocs_arr[0] !== undefined) {
+            insMoDoc.cid = moDocs_arr[0].cid + 1;
+          } else {
+            insMoDoc.cid = 0;
+          }
+
+          db.collection(contentCollection).insert(insMoDoc, function (err) {
+            if (err) { logg.me('error', __filename + ':44 ' + err); }
+          });
         });
 
       }
