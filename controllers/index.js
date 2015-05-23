a334f127fc0145204e33499bedae63e16ac6b78d
smikodanic <smikodanic@gmail.com>
2015-05-23 22:44:20 +0200

    homepage template

--- controllers/index.js
+++ controllers/index.js
@@ -7,8 +7,16 @@ var nodedump = require('nodedump').dump;
 
 module.exports = function (router) {
 
-  router.get('/', function (req, res, next) {
-    res.render('public/index');
+  router.get('/', function (req, res) {
+
+    var vdata = {
+      title: 'Find Ads - Search Free Classifieds',
+      desc: 'Find Ads is your help when searching free classified ads. All adverts are sorted in categories and subcategories.',
+      keywords: 'search classifieds, find ads, search adverts, clasifieds directory',
+      h1: 'Find Ads'
+    };
+
+    res.render('public/index', vdata);
   });
 
 };
