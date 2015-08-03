772590a4e4168a4491b562a22ae94396f4afe46d
smikodanic <smikodanic@gmail.com>
2015-08-03 15:22:05 +0200

    disable cron to all tasks

--- controllers/admin/crawlcontent/tasks.js
+++ controllers/admin/crawlcontent/tasks.js
@@ -123,20 +123,6 @@ module.exports = function (router) {
 
   });
 
-  //delete task from MongoDB
-  router.get('/delete/:id', function (req, res) {
-
-    //check if username and password are good in session storage
-    var sess_tf = login.checksess_user_pass(req);
-
-    if (sess_tf) {
-      taskContent_model.deleteTask(req, res);
-    } else {
-      res.redirect('/admin');
-    }
-
-  });
-
 
   /**
    * Start crawling task from browser
--- controllers/admin/robot/tasks.js
+++ controllers/admin/robot/tasks.js
@@ -108,5 +108,93 @@ module.exports = function (router) {
   });
 
 
+    //disable all tasks
+  router.get('/disable', function (req, res) {
+
+    //check if username and password are good in session storage
+    var sess_tf = login.checksess_user_pass(req);
+
+    if (sess_tf) {
+      robotTasks_model.disableTasks(res);
+    } else {
+      res.redirect('/admin');
+    }
+
+  });
+
+
+  /**
+   * Start crawling task from browser
+   * To start crawling tasks from command line use: $pm2 start 0crawler/robot/linkQueue_cli.js 2
+   * @param {number} :task_id -id of the task from 'robot_tasks' collection 
+   */
+  router.get('/start/:task_id', function (req, res) {
+
+    //check username and password
+    var sess_tf = login.checksess_user_pass(req);
+
+    //id from req e.g. from URI
+    var task_id = parseInt(req.params.task_id, 10); //use parseint to convert string into number
+
+    //callback: output crawling results to browser
+    var cb_outResults = {
+      send: function (rezult) {
+        res.write(rezult);
+      },
+      end: function () {
+        res.end();
+      }
+    };
+
+    if (sess_tf) {
+
+      /* start crawling in same NodeJS process */
+      var MongoClient = require('mongodb').MongoClient;
+      var logg = require('libraries/loggLib');
+      var settings = require('settings/admin.js');
+      var dbName = settings.mongo.dbName;
+      var collName_tasksCnt = settings.mongo.dbColl_tasksCnt;
+
+      // Connect to MongoDB 'contentTasks' collection to get 'pollScript' value
+      MongoClient.connect(dbName, function (err, db) {
+        if (err) { logg.me('error', __filename + ':175 ' + err); }
+
+        db.collection(collName_tasksCnt).find({"id": task_id}).toArray(function (err, contentTasks_arr) { //get pollScript for a given task ID
+          if (err) { logg.me('error', __filename + ':178 ' + err); }
+
+          //activate poll script for content crawling: 0crawler/crawlcontent/polling/linkQueue_polling.js
+          var pollContentLinks = require('0crawler/crawlcontent/polling/' + contentTasks_arr[0].pollScript);
+          pollContentLinks.start(task_id, cb_outResults);
+
+          db.close(); //close DB connection
+        });
+
+      });
+
+
+
+    } else {
+      res.redirect('/admin');
+    }
+
+  });
+
+
+  //stop crawling
+  router.get('/stop', function (req, res) {
+
+    //check username and password
+    var sess_tf = login.checksess_user_pass(req);
+
+    if (sess_tf) {
+      clearInterval(global.intId);
+      res.redirect('/admin/crawlcontent/tasks');
+    } else {
+      res.redirect('/admin');
+    }
+
+  });
+
+
 
 }; //module end
